b3ee6dcd7c14d26ad706a63b0dfae1fe4eef9b07
Paul Kassar <paulkassar@gmail.com>
2017-06-13 14:46:11 +0100

    more tests for Transactions

--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -4,23 +4,29 @@ describe Transaction do
   subject(:transactions) { described_class.new }
 
   before do
-     @date = Date.today.strftime('%d-%m-%y')
-   end
+    @date = Date.today.strftime('%d-%m-%y')
+  end
 
-   new_balance = 1000
-     amount = 100
-     subject(:credit) { described_class.new(amount, new_balance) }
-     subject(:debit) { described_class.new(-amount, new_balance) }
+  new_balance = 1000
+  amount = 100
+  subject(:credit) { described_class.new(amount, new_balance) }
+  subject(:debit) { described_class.new(-amount, new_balance) }
 
   context "A credit of £100" do
     it "Adds a transaction at the date it happened to the log" do
       expect(credit.activity[:date]).to eq @date
     end
-  end
 
-  it 'records the amount credited' do
+    it 'records the amount credited' do
       expect(credit.activity[:credit]).to eq 100
     end
 
+    it 'records the amount debited' do
+      expect(credit.activity[:debit]).to eq 0
+    end
 
+    it 'records the new balance' do
+      expect(credit.activity[:balance]).to eq 1000
+    end
+  end
 end
